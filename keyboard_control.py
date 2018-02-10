c4a14fed99cfcb959996d7962d454938ff27025e
NeilLambeth <neillambeth@gmail.com>
2018-02-10 16:01:12 +0000

    Update keyboard_control.py

--- keyboard_control.py
+++ keyboard_control.py
@@ -1,11 +1,10 @@
 import curses
 import time
-from sideboard import *
+import redboard
 stdscr = curses.initscr()
 curses.noecho()
 #curses.cbreak()
 stdscr.keypad(True)
-
 
 print("\r")
 print("W = Forwards\r")
@@ -13,11 +12,15 @@ print("S = Backwards\r")
 print("A = Left\r")
 print("D = Right\r")
 print("Spacebar = Stop\r")
+print("T = Turbo")
 
 time.sleep(3)
 
 
 def main(stdscr):
+    motor1 = 0
+    motor2 = 0
+    turbo = False
     
     keypress = 0
     up = 0
@@ -26,7 +29,7 @@ def main(stdscr):
         
         curses.halfdelay(1)
         c = stdscr.getch()
-        #print(c)
+        #print(c)  # Uncomment to show keypresses
         #print("\r")
         
         if c == ord('w'):
@@ -34,56 +37,90 @@ def main(stdscr):
             print("Forward")
             print("\r")
             stop = 0
-            M2 (100)
-            M1 (100)
-            
+            #redboard.M2 (100)
+            #redboard.M1 (100)
+            motor1 = 100
+            motor2 = 100
 
         elif c == ord('s'):
             keypress = 1
             stop = 0
             print("Backwards")
             print("\r")
-            M2 (-100)
-            M1 (-100)
+            #redboard.M2 (-100)
+            #redboard.M1 (-100)
+            motor1 = -100
+            motor2 = -100
 
         elif c == ord('a'):
             keypress = 1
             stop = 0
             print("Left")
             print("\r")
-            M2 (-100)
-            M1 (100)
-
+            #redboard.M2 (-100)
+            #redboard.M1 (100)
+            motor1 = 100
+            motor2 = -100
+			
         elif c == ord('d'):
             keypress = 1
             stop = 0
             print("Right")
             print("\r")
-            M2 (100)
-            M1 (-100)
+            #redboard.M2 (100)
+            #redboard.M1 (-100)
+            motor1 = -100
+            motor2 = 100
+            
             
         elif c == 32:  # Spacebar
             keypress = 1
             stop = 0
             print("Stop")
             print("\r")
-            M2 (0)
-            M1 (0)    
-                            
+            #redboard.M2 (0)
+            #redboard.M1 (0)    
+            motor1 = 0
+            motor2 = 0                
+
+        elif c == ord('t') and turbo == False :
+            keypress = 1
+            stop = 0
+            turbo = True
+            print ("Turbo on")
+            print("\r")
+
+        elif c == ord('t') and turbo == True:
+            keypress = 1
+            stop = 0
+            turbo = False
+            print ("Turbo off")
+            print("\r")
 
         if c == -1:
             stop += 1
             #print(stop)
             #print("\r")
-            #up = 0
+          
 
         if keypress == 1 and stop > 6:
             keypress = 0
             stop = 0
             print("Stop----------------------------------------")
             print("\r")
-            M2 (0)
-            M1 (0)
+            #redboard.M2 (0)
+            #redboard.M1 (0)
+            motor1 = 0
+            motor2 = 0
+
+
+        if turbo == True:
+            redboard.M1(motor1)
+            redboard.M2(motor2)
+
+        elif turbo == False:
+            redboard.M1(motor1 / 2)  # divide motor speed by 2 
+            redboard.M2(motor2 / 2)  # divide motor speed by 2
           
 curses.wrapper(main)    
             
