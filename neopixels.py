61f7a6c3646f160dc4aa04cdd4fe5e0d755f6e9b
NeilLambeth <neillambeth@gmail.com>
2018-02-14 00:08:35 +0000

    Added Adafruit examples

--- neopixels.py
+++ neopixels.py
@@ -12,8 +12,8 @@ print("neopixels loaded")
 
 
 # LED strip configuration:
-LED_COUNT      = 9     # Number of LED pixels.
-LED_PIN        = 12      # GPIO pin connected to the pixels (18 uses PWM!).
+LED_COUNT      = 1    # Number of LED pixels.
+LED_PIN        = 12      # GPIO pin connected to the pixels (12 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
@@ -33,23 +33,35 @@ def clear():
 		strip.setPixelColor(i,0)
 		strip.show()
 
-def blue():
-        strip.setPixelColorRGB(0, 0,0,255) 
-        strip.show()
+def blue(value=None):
+    if value is None:
+        value = 255
+    strip.setPixelColorRGB(0, 0,0,value) 
+    strip.show()
 
-def green():
-        strip.setPixelColorRGB(0, 0,255,0) 
-        strip.show()
+def green(value=None):
+    if value is None:
+        value = 255
+    strip.setPixelColorRGB(0, 0,value,0) 
+    strip.show()
 
-def red():
-        strip.setPixelColorRGB(0, 255,0,0) 
-        strip.show()        
+def red(value=None):
+    if value is None:
+        value = 255
+    strip.setPixelColorRGB(0, value,0,0) 
+    strip.show()
+
+def white(value=None):
+    if value is None:
+        value = 255
+    strip.setPixelColorRGB(0, value,value,value) 
+    strip.show()        
 
 def setColour(P,R,G,B):
         strip.setPixelColorRGB(P,R,G,B) 
         strip.show()
 
-def heartbeat():
+def heartBeat():
         red = 0
         while True:
                 #Fade up one red LED
@@ -67,7 +79,7 @@ def heartbeat():
                     strip.show()
                     time.sleep(0.01)        
 
-def knightrider():
+def knightRider():
     while True:
         for i in range(1,strip.numPixels()-1):
                 strip.setPixelColorRGB(i, 255,0,0) 
@@ -81,6 +93,78 @@ def knightrider():
                 time.sleep(0.1)
                 clear()
 
+
+def colorWipe(strip, color, wait_ms=50):
+	"""Wipe color across display a pixel at a time."""
+	for i in range(strip.numPixels()):
+		strip.setPixelColor(i, color)
+		strip.show()
+		time.sleep(wait_ms/1000.0)
+
+def theaterChase(strip, color, wait_ms=50, iterations=10):
+	"""Movie theater light style chaser animation."""
+	for j in range(iterations):
+		for q in range(3):
+			for i in range(0, strip.numPixels(), 3):
+				strip.setPixelColor(i+q, color)
+			strip.show()
+			time.sleep(wait_ms/1000.0)
+			for i in range(0, strip.numPixels(), 3):
+				strip.setPixelColor(i+q, 0)
+
+def wheel(pos):
+	"""Generate rainbow colors across 0-255 positions."""
+	if pos < 85:
+		return Color(pos * 3, 255 - pos * 3, 0)
+	elif pos < 170:
+		pos -= 85
+		return Color(255 - pos * 3, 0, pos * 3)
+	else:
+		pos -= 170
+		return Color(0, pos * 3, 255 - pos * 3)
+
+def rainbow(strip, wait_ms=20, iterations=1):
+	"""Draw rainbow that fades across all pixels at once."""
+	for j in range(256*iterations):
+		for i in range(strip.numPixels()):
+			strip.setPixelColor(i, wheel((i+j) & 255))
+		strip.show()
+		time.sleep(wait_ms/1000.0)
+
+def rainbowCycle(strip, wait_ms=20, iterations=5):
+	"""Draw rainbow that uniformly distributes itself across all pixels."""
+	for j in range(256*iterations):
+		for i in range(strip.numPixels()):
+			strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
+		strip.show()
+		time.sleep(wait_ms/1000.0)
+
+def theaterChaseRainbow(strip, wait_ms=50):
+	"""Rainbow movie theater light style chaser animation."""
+	for j in range(256):
+		for q in range(3):
+			for i in range(0, strip.numPixels(), 3):
+				strip.setPixelColor(i+q, wheel((i+j) % 255))
+			strip.show()
+			time.sleep(wait_ms/1000.0)
+			for i in range(0, strip.numPixels(), 3):
+				strip.setPixelColor(i+q, 0)
+
+def demo():
+    print ('Press Ctrl-C to quit.')
+    while True:
+        print ('Color wipe animations.')
+        colorWipe(strip, Color(255, 0, 0))  # Red wipe
+        colorWipe(strip, Color(0, 255, 0))  # Blue wipe
+        colorWipe(strip, Color(0, 0, 255))  # Green wipe
+        print ('Theater chase animations.')
+        theaterChase(strip, Color(127, 127, 127))  # White theater chase
+        theaterChase(strip, Color(127,   0,   0))  # Red theater chase
+        theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
+        print ('Rainbow animations.')
+        rainbow(strip)
+        rainbowCycle(strip)
+        theaterChaseRainbow(strip)
 
 
 # Main program logic follows:
